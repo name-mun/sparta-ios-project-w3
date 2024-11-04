9a9192b7be0b118caee64f2dffb370045fc9ffc5
jamong-mini <jamong.alsl@gmail.com>
2024-11-04 10:59:54 +0900

    Initial commit: 프로젝트 설정 및 숫자 야구 게임 기본 구조 추가
    - main.swift 파일 생성 및 레벨 선택을 통한 게임 시작 로직 추가
    - Lv_1.swift ~ Lv_6.swift 파일 생성 및 레벨별 기능 구현을 위한 함수 틀 추가
    - README에 협업 가이드 및 레벨별 구현 가이드 작성

---
+++ Week2-BaseballGame/Week2-BaseballGame/main.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func startGame() {
+    print("레벨을 선택하세요 (1, 2, 4, 5, 6):")
+    
+    if let input = readLine(), let level = Int(input) {
+        switch level {
+        case 1:
+            levelOne()
+        case 2:
+            levelTwo()
+        case 3:
+            levelThree()
+        case 4:
+            levelFour()
+        case 5:
+            levelFive()
+        case 6:
+            levelSix()
+        default:
+            print("유효하지 않은 레벨입니다. 1~6까지를 선택해주세요.")
+        }
+    } else {
+        print("잘못된 입력입니다.")
+    }
+}
+
+// 게임 시작
+startGame()
