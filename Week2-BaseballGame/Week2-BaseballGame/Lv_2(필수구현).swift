71ebc28baea2e67a1e06302a81ee9b975b7ac58d
name-mun <tains9153@gmail.com>
2024-11-05 16:02:27 +0900

    ✨ 입력값 예외 처리 구현

--- Week2-BaseballGame/Week2-BaseballGame/Lv_2(필수구현).swift
+++ Week2-BaseballGame/Week2-BaseballGame/Lv_2(필수구현).swift
@@ -16,6 +16,63 @@
 
 import Foundation
 
-func levelTwo() {
+func inputErrorCheck(_ input: String?) -> Bool {
+    var numberCount = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    // 입력값이 3개가 아닌경우 예외처리
+    if input!.count != 3 {
+        return false
+    }
     
+    // 입력값이 숫자가 아니거나 0이 있는 경우 예외처리
+    for i in input! {
+        if Int(String(i)) == nil || Int(String(i)) == 0 {
+            return false
+        }
+        numberCount[Int(String(i))!-1] += 1
+    }
+    
+    // 입력값에 같은 수가 2번 있는 경우 예외처리
+    for i in numberCount {
+        if i > 1 {
+            return false
+        }
+    }
+    
+    return true
+}
+
+func levelTwo() {
+    // 랜덤값을 저장할 set 생성
+    var randomNumberSet = Set<Int>()
+    
+    // 스트라이크와 볼을 저장할 변수 생성
+    var strikeCount = 0
+    var ballCount = 0
+    
+    // randomNumberSet의 크기가 3이 될 때까지 실행
+    while (randomNumberSet.count < 3) {
+        // randomNumberSet에 랜덤 숫자 추가
+        randomNumberSet.insert(Int.random(in: 1 ... 9))
+    }
+    // randomNumberSet을 배열로 저장
+    let randomNumberArray = randomNumberSet.map { $0 }
+
+    print("< 게임을 시작합니다 >")
+    
+    
+    // 정답을 맞출 때까지 실행
+    while strikeCount != 3 {
+        print("숫자를 입력하세요")
+        
+        var input = readLine()
+        
+        if inputErrorCheck(input) == false {
+            print("올바르지 않은 입력값입니다\n")
+            continue
+        }
+        
+
+        
+    }
 }
